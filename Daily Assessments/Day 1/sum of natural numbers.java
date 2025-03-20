62a121f53c00d4ec2d209e275d655bfc1d68d866
Yaswanth M <116774291+yashutoxie@users.noreply.github.com>
2025-03-20 14:42:51 +0530

    Create sum of natural numbers.java

---
+++ Daily Assessments/Day 1/sum of natural numbers.java
@@ -0,0 +1,11 @@
+import java.io.*;
+ 
+class Main { 
+    public static void main(String[] args)
+    {
+        int N = 5;
+        System.out.println("Finding Sum of " + N); 
+        int sum = (N*(N+1))/2; 
+        System.out.println("Sum of Natural Numbers = " + sum);
+    }
+}
