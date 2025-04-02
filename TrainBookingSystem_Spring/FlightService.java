ca856e0a7f55863c230e5a2acdc0e1be0c1e140b
Yaswanth M <116774291+yashutoxie@users.noreply.github.com>
2025-04-02 12:38:26 +0530

    Create FlightService.java

---
+++ TrainBookingSystem_Spring/FlightService.java
@@ -0,0 +1,11 @@
+package com.spring.TravelBookingSystem;
+
+import org.springframework.stereotype.Component;
+
+@Component("flightservice")
+class FlightService implements TransportService {
+	@Override
+    public void BookTicket(String source, String destination) {
+        System.out.println("Flight ticket booked from " + source + " to " + destination);
+    }
+}
