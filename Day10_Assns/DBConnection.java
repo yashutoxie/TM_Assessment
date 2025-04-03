efa4c59859ee12e81abad67602079659e55f5131
Yaswanth M <116774291+yashutoxie@users.noreply.github.com>
2025-04-03 07:07:45 +0530

    Create DBConnection.java

---
+++ Day10_Assns/DBConnection.java
@@ -0,0 +1,12 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/ecommerce";
+    private static final String USER = "root"; 
+    private static final String PASSWORD = "pasword"; 
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
